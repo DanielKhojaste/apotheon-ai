3c8862becb167a3cfaa554708514d9344de53062
DanielKhojaste <danielkhojaste101@gmail.com>
2026-08-23 21:15:32 -0400

    fix(cms): use defineField in author schema

--- studio/schemaTypes/authorType.ts
+++ studio/schemaTypes/authorType.ts
@@ -1,24 +1,26 @@
-import {defineType} from 'sanity'
+import {defineField, defineType} from 'sanity'
 
 export const authorType = defineType({
-  name: 'author',
-  title: 'Author',
-  type: 'document',
-  fields: [
-    defineType({
-      name: 'id',
-      title: 'ID',
-      type: 'string',
-    }),
-    defineType({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    }),
-    defineType({
-      name: 'profilePicture',
-      title: 'Profile Picture',
-      type: 'image',
-    }),
-  ],
+	name: 'author',
+	title: 'Author',
+	type: 'document',
+	fields: [
+		defineField({
+			name: 'id',
+			title: 'ID',
+			type: 'string',
+		}),
+
+		defineField({
+			name: 'name',
+			title: 'Name',
+			type: 'string',
+		}),
+
+		defineField({
+			name: 'profilePicture',
+			title: 'Profile Picture',
+			type: 'image',
+		}),
+	],
 })
